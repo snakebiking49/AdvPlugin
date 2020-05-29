f7f2427ef46510f13734ed1ac78d7e16bd36b019
karee <karee@LAPTOP-EAN2FRSQ.gateway>
2020-05-29 12:06:08 +1000

    Added main class

---
+++ src/main/java/AdvPlugin/AdvPLugin.java
@@ -0,0 +1,14 @@
+package AdvPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+public class AdvPLugin extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	public void onDisable()
+	{
+		
+	}
+}
