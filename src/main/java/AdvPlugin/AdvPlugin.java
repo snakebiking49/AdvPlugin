2467fd1cdce7c386bdfab969f386a6eae1640eb2
kareemBarton <kareem.cort.barton@gmail.com>
2020-06-04 11:29:46 +1000

    Stable release, added a ton of feature and new commands, teleportation in and out of the world and areas has been added, gamemode changing for players has been added, and a ton of other less important optimizations.

--- src/main/java/AdvPlugin/AdvPlugin.java
+++ src/main/java/AdvPlugin/AdvPlugin.java
@@ -2,22 +2,36 @@ package AdvPlugin;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 public class AdvPlugin extends JavaPlugin
 {
 	List <Player> playerlist;//online players
 	List <Player> AdvPlayersList;//online players who have joined the event
-	
+	Boolean event = false;
+	World adventureWorld = this.getServer().getWorld("AdventureWorld");
+	Location group1 = new Location(this.getServer().getWorld("AdventureWorld"),0,200,0);
+	Location group2 = new Location(this.getServer().getWorld("AdventureWorld"),0,200,0);
+	Location group3 = new Location(this.getServer().getWorld("AdventureWorld"),0,200,0);
+	Location group4 = new Location(this.getServer().getWorld("AdventureWorld"),0,200,0);
 	@Override
 	public void onEnable()
 	{
 		getLogger().info("The AdvPlugin is being enabled!");
 		new AdvEventListener(this);
 		this.getCommand("setGroup").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("setRank").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("joinAdv").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("createGame").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("destroyGame").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("leaveAdv").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("setGroupHome").setExecutor(new AdvCommandExecutor(this));
+		this.getCommand("startGame").setExecutor(new AdvCommandExecutor(this));
 		for (Player player : Bukkit.getServer().getOnlinePlayers()) 
 		{	
-		    playerlist.add(player);   
+		    playerlist.add(player);  
 		}
 		
 	
